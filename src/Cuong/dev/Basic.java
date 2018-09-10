bb708828bff14f5888c25006cae45804e1d801bb
cuong <cuong.nguyenkhac@students.edu.vn>
2018-09-10 21:49:24 +0700

    for_loop

--- src/Cuong/dev/Basic.java
+++ src/Cuong/dev/Basic.java
@@ -2,42 +2,10 @@ package Cuong.dev;
 
 public class Basic {
 	public static void main(String[] agrs) {
-		int n=5;
-	       switch(n) {
-	       case 0:
-	    	   System.out.println("zero");
-	    	   break;
-	       case 1:
-	    	   System.out.println("one");
-	    	   break;
-	       case 2:
-	    	   System.out.println("two");
-	           break;
-	       case 3:
-	    	   System.out.println("three");
-	    	   break;
-	       case 4:
-	    	   System.out.println("four");
-	    	   break;
-	       case 5:
-	    	   System.out.println("five");
-	    	   break;
-	       case 6:
-	    	   System.out.println("six");
-	    	   break;
-	       case 7:
-	    	   System.out.println("seven");
-	    	   break;
-	       case 8:
-	    	   System.out.println("eight");
-	    	   break;
-	       case 9:
-	    	   System.out.println("nine");
-	    	   break;
-	       case 10:
-	    	   System.out.println("ten");
-	    	default:
-	    		System.out.println("khong co trong case");
-	       }
+		 int tong=0;
+	      for(int i=0;i<=100;i++) {
+	    	  tong=tong+i;
+	      }
+	     System.out.println("tong 100 so dau tien:"+tong);
 	   }
 }  
